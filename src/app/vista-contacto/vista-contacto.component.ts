abce275e8ea7bd0361332ea11ac0cd9e7fdcb43b
QuantumInformatik <juanguillermoalarcon@gmail.com>
2020-03-24 21:03:54 -0500

    se consume api rest para el formulario de contacto

--- src/app/vista-contacto/vista-contacto.component.ts
+++ src/app/vista-contacto/vista-contacto.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Contacto } from './contacto';
+import { ContactoService } from './contacto.service';
 
 
 @Component({
@@ -9,13 +11,23 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class VistaContactoComponent implements OnInit {
 
-  constructor(public translate: TranslateService) {
+  public objContacto: Contacto = new Contacto();
+
+  constructor(public translate: TranslateService, private contactoService: ContactoService) {
     translate.addLangs(['en', 'es']);
     translate.setDefaultLang('en');
   }
 
   switchLang(lang: string) {
     this.translate.use(lang);
+  }
+
+  public enviarMensaje(): void{
+    console.log('click');
+    console.log(this.objContacto);
+    this.contactoService.enviarMensaje(this.objContacto).subscribe(
+      response => alert('hola')
+    );
   }
 
   ngOnInit(): void {
